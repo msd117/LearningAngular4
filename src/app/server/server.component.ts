a065f98c636aca79586056587728bc1016cf8741
swxuser <msd117@yahoo.com>
2018-08-04 18:54:53 +0530

    ngif,ngfor,ngstyle,ngclass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,13 +1,28 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'app-server',
-    templateUrl:'./server.component.html'
-
+    // selector:'app-server',
+     // selector: 'app-server', //for selecting using elemnts
+  // selector: '[app-server]', //for selecting using as attributes
+  selector: '.app-server', //for selecting using as class
+  
+    templateUrl:'./server.component.html',
+    styles:[`
+    .online{
+        color:white;
+    }
+    `]
+    
 })
 
 export class ServerComponent{
     serverID=10;
     serverStatus='offline';
+    constructor(){
+        this.serverStatus= Math.random()>0.5? 'Online':'Offline';
+    }
+    getColor(){
+        return this.serverStatus==='Online'?'green':'red';
+    }
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //for selecting using elemnts
+  // selector: '[app-servers]', //for selecting using as attributes
+  // selector: '.app-servers', //for selecting using as class
+  
+  
+  
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
@@ -9,17 +14,22 @@ export class ServersComponent implements OnInit {
 
   allowNewServer=false;
   serverCreationStatus="Server is not created"
+  isServercreated=false;
   serverName="Testserver"
+  servers=["testServer","Testserver 1"]
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer=true;
     }, 2000);
+
    }
 
   ngOnInit() {
   }
   onCreateServer(){
+    this.isServercreated=true
+    this.servers.push(this.serverName);
     this.serverCreationStatus="Server is Created and the name is  " + this.serverName;
   }
   
